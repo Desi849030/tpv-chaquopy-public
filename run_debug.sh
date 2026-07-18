1388afdd3e8ec69677f6f47a9baef35e2799bc08
Desi849030 <tu@email.com>
2026-07-17 20:16:25 -0400

    chore: limpiar repo - secretos, basura, dev scripts, db, logs

--- run_debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~/tpv-chaquopy-public
-source agent_env/bin/activate
-export PYTHONPATH="$(pwd)/app/src/main/python"
-export FLASK_DEBUG=1
-python app/src/main/python/start_server.py
--- run_tpv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ~/tpv-chaquopy-public
-source agent_env/bin/activate
-python run_tpv.py
